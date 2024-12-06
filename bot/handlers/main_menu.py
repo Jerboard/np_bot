993733c8914c2af28626aa7bc9fb6d311991c7dc
dgush <dgushch@gmail.com>
2024-12-06 18:33:01 +0400

    beta 1.1.14

--- bot/handlers/main_menu.py
+++ bot/handlers/main_menu.py
@@ -15,7 +15,7 @@ from enums import CB, Command, UserState, Action
 
 
 # 781602290203
-# test update github 3
+# test update github 4
 # Обработчик команды /start
 @dp.message(CommandStart())
 async def start(msg: Message, state: FSMContext):
